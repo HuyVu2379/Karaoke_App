041061bfdd2ac799953e776b509cf48e6a5122fb
huynhtannha2003 <127366885+huynhtannha2003@users.noreply.github.com>
2024-04-22 10:05:27 +0700

    Add files via upload

---
+++ src/main/java/dao/Impl/HoaDonImpl.java
@@ -0,0 +1,72 @@
+package implDao;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import entity.HoaDon;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class HoaDonImpl implements HoaDonDAO{
+	private EntityManager em;
+	EntityTransaction tx;
+
+	public HoaDonImpl() {
+		em = Persistence.createEntityManagerFactory("ssql").createEntityManager();
+	}
+	@Override
+	public List<HoaDon> getAllHoaDon() throws RemoteException {
+		String query = "Select hd from HoaDon hd";
+		List<HoaDon> listHd = new ArrayList<HoaDon>();
+		listHd = em.createQuery(query).getResultList();
+		return listHd;
+	}
+	@Override
+	public List<HoaDon> getHoaDonPaged(int page, int pageSize) throws RemoteException {
+		String query = "Select hd from HoaDon hd";
+		List<HoaDon> listHd = new ArrayList<HoaDon>();
+		listHd = em.createQuery(query).setFirstResult(page).setMaxResults(pageSize).getResultList();
+		return listHd;
+	}
+	@Override
+	public List<HoaDon> getHoaDonPagedByMaHoaDon(int page, int pageSize, String maHd) throws RemoteException {
+		String query = "Select hd from HoaDon hd where hd.maHoaDon = :maHd";
+		List<HoaDon> listHd = new ArrayList<HoaDon>();
+		listHd = em.createQuery(query).setParameter("maHd", maHd).setFirstResult(page).setMaxResults(pageSize)
+				.getResultList();
+		return listHd;
+	}
+	@Override
+	public List<HoaDon> getHoaDonPagedByTenKhachHang(int page, int pageSize, String tenKh) throws RemoteException {
+		String query = "Select hd from HoaDon hd where hd.khachHang.tenKhachHang = :tenKh";
+		List<HoaDon> listHd = new ArrayList<HoaDon>();
+		listHd = em.createQuery(query).setParameter("tenKh", tenKh).setFirstResult(page).setMaxResults(pageSize)
+				.getResultList();
+		return listHd;
+	}
+	@Override
+	public List<HoaDon> getHoaDonPagedByDateRange(int page, int pageSize, Date ngayBatDau, Date ngayKetThuc) throws	RemoteException {
+	
+		String query = "Select hd from HoaDon hd where hd.NgayThanhToan between :ngayBatDau and :ngayKetThuc";
+		List<HoaDon> listHd = new ArrayList<HoaDon>();
+		listHd = em.createQuery(query).setParameter("ngayBatDau", ngayBatDau).setParameter("ngayKetThuc", ngayKetThuc)
+				.setFirstResult(page).setMaxResults(pageSize).getResultList();
+		return listHd;
+	}
+	@Override
+	public HoaDon getHoaDonByMaPhong(String maPhong) throws RemoteException {
+		String query = "Select hd from HoaDon hd where hd.phong.maPhong = :maPhong";
+		HoaDon hd = (HoaDon) em.createQuery(query).setParameter("maPhong", maPhong).getSingleResult();
+		return hd;
+	}
+	@Override
+	public HoaDon getHoaDonByMaHoaDon(String maHd) throws RemoteException {
+		String query = "Select hd from HoaDon hd where hd.maHoaDon = :maHd";
+		HoaDon hd = (HoaDon) em.createQuery(query).setParameter("maHd", maHd).getSingleResult();
+		return hd;
+	}
+	
+}
---
+++ src/main/java/dao/Impl/KhachHangDaoImpl.java
@@ -0,0 +1,84 @@
+package implDao;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+
+import entity.KhachHang;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class KhachHangDaoImpl implements KhachHangDAO {
+	private EntityManager em;
+	EntityTransaction tx;
+
+	public KhachHangDaoImpl() {
+		em = Persistence.createEntityManagerFactory("ssql").createEntityManager();
+	}
+	
+	@Override
+	// lấy danh sách khách hàng
+	public List<KhachHang> getAllKhachHang() throws RemoteException {
+		String query = "Select kh from KhachHang kh";
+		List<KhachHang> listKh = new ArrayList<KhachHang>();
+		listKh = em.createQuery(query).getResultList();
+		return listKh;
+	}
+	@Override
+	//lấy  khách hàng theo số phone
+	public KhachHang getKhachHangByPhone(String phone) throws RemoteException {
+		String query = "Select kh from KhachHang kh where kh.soDienThoai = :phone";
+		KhachHang kh = (KhachHang) em.createQuery(query).setParameter("phone", phone).getSingleResult();
+		return kh;
+	}
+	@Override
+    //tạo khách hàng
+	public boolean createKhachHang(KhachHang kh) {
+		tx = em.getTransaction();
+		try {
+			tx.begin();
+			em.persist(kh);
+			tx.commit();
+			return true;
+		} catch (Exception e) {
+			tx.rollback();
+			e.printStackTrace();
+		}
+		return true;
+	}
+	@Override
+	//cập nhật khách hàng
+	public boolean updateKhachHang(KhachHang kh, String phone) {
+        tx = em.getTransaction();
+        try {
+            tx.begin();
+            KhachHang khachHang = em.find(KhachHang.class, phone);
+            khachHang.setMaKhachHang(kh.getMaKhachHang());
+            khachHang.setTenKhachHang(kh.getTenKhachHang());
+            khachHang.setSdt(kh.getSdt());
+            em.merge(khachHang);
+            tx.commit();
+            } catch (Exception e) {
+            	tx.rollback();
+            	e.printStackTrace();
+            }
+           return true;
+	}
+	@Override
+	//Tìm kiếm khách hàng theo têm khách hàng
+	public List<KhachHang> searchKhachHang(String tenKh) throws RemoteException {
+		String query = "Select kh from KhachHang kh where kh.tenKhachHang = :tenKh";
+		List<KhachHang> listKh = new ArrayList<KhachHang>();
+		listKh = em.createQuery(query).setParameter("tenKh", tenKh).getResultList();
+		return listKh;
+	}
+
+	@Override
+	public boolean deleteKhachHang(String phone) throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+}
